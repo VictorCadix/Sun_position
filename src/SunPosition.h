1c10a40160963bd2e60ab2d97fc36101d4240e0c
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2018-02-05 19:55:41 +0100

    JulianDays fonction

--- src/SunPosition.h
+++ src/SunPosition.h
@@ -9,3 +9,23 @@ typedef struct Time {
 	int seconds;
 };
 
+
+double JulianDays(Time time) {
+	int Y = time.year;
+	int M = time.month;
+	int D = time.day;
+	int hour = time.hours;
+	int min = time.minutes;
+	int sec = time.seconds;
+
+	int JDN;	//Julian day number
+	double JD;	//Julian day
+	double eJD;	//elapsed days since J2000.0
+
+	JDN = (1461.0 * (Y + 4800.0 + (M - 14.0) / 12.0)) / 4.0 + (367.0 * (M - 2.0 - 12.0 * ((M - 14.0) / 12.0))) / 12.0 - (3.0 * ((Y + 4900.0 + (M - 14.0) / 12.0) / 100.0)) / 4.0 + D - 32075.0;
+	JD = JDN + (hour - 12.0) / 24.0 + min / 1440.0 + sec / 86400.0;
+	eJD = JD - 2451545.0;
+
+	return eJD;
+}
+
