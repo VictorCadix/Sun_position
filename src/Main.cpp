9f39928db389bda5704d6578a0c74794fd0c4c8a
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2018-02-05 19:56:12 +0100

    test of JulianDays()

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,4 +7,18 @@ using namespace std;
 
 void main() {
 
+	Time time;
+	time.year = 2018;
+	time.month = 2;
+	time.day = 5;
+	time.hours = 18;
+	time.minutes = 46;
+	time.seconds = 40;
+
+	double eJD = JulianDays(time);
+
+	cout.precision(10);
+	cout << "elasped Julian Days: " << eJD << endl;
+
+	_getch();
 }
