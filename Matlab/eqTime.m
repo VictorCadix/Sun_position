2782089229c4b79d7afadeebfc043eb902c1eb12
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2018-02-12 00:27:29 +0100

    equation of time plot added

--- Matlab/eqTime.m
+++ Matlab/eqTime.m
@@ -1,7 +1,14 @@
 function [ eqtime ] = eqTime( fract_year )
 %time in minutes
 %input in rad
+n = 0:(2*pi/365):(2*pi);
+time = 229.18*(0.000075 + 0.001868*cos(n) - 0.032077*sin(n) - 0.014615*cos(2*n)- 0.040849*sin(2*n)); 
 eqtime = 229.18*(0.000075 + 0.001868*cos(fract_year) - 0.032077*sin(fract_year) - 0.014615*cos(2*fract_year)- 0.040849*sin(2*fract_year)); 
 
+figure('Name','eqTime','NumberTitle','off');
+plot (n,time)
+hold on
+plot(fract_year,eqtime,'*')
+hold off
 end
 
