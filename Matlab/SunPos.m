c73c6a38d0eabf94f5ebf7631d3dbbab8767bfa6
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2018-02-12 00:27:51 +0100

    Funcitons test

--- Matlab/SunPos.m
+++ Matlab/SunPos.m
@@ -1,3 +1,12 @@
 clear %comment if needed
-day = 30;
+
+Y = 2018
+M = 2
+D = 12
+
+time = [Y M D 12 0 0]
+day = 43;
+
 decl = declination(day)
+fractYear_rad = fractionalYear(time(4),day)
+eqtime = eqTime(fractYear_rad)
