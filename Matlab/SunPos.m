291aa08b0e82f15483cc6f846bba2cb26fc66643
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2018-02-12 12:07:19 +0100

    hour angle function

--- Matlab/SunPos.m
+++ Matlab/SunPos.m
@@ -7,6 +7,12 @@ D = 12
 time = [Y M D 12 0 0]
 day = 43;
 
+%position
+longitude = -3.7269;
+timezone = +1;
+
 decl = declination(day)
 fractYear_rad = fractionalYear(time(4),day)
 eqtime = eqTime(fractYear_rad)
+tst = trueSolarTime(eqtime,longitude,timezone,time)
+ha = hourAngle(tst)
---
+++ Matlab/hourAngle.m
@@ -0,0 +1,5 @@
+function [ ha ] = hourAngle( true_solar_time )
+% hour angle in degrees
+ha = (true_solar_time / 4) - 180;
+end
+
