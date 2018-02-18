4a2ff827ebf59bebd50209269e16ab9dce414194
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2018-02-18 15:24:53 +0100

    Correct result

--- Matlab/sunCoordinates.m
+++ Matlab/sunCoordinates.m
@@ -1,14 +1,25 @@
 function [ altitude,azimuth ] = sunCoordinates( latitude,declination,hour_angle )
 
-% altitude calculation
+% altitude calculation NOAA
 cos_zenith = sind(latitude)*sind(declination) + cosd(latitude)*cosd(declination)*cosd(hour_angle);
 zenith = acosd(cos_zenith);
 altitude = 90-zenith;
 
 % azimuth calculation
+aux = asind((-sind(hour_angle)*cosd(declination))/(sind(zenith)));
+azimuth = 180-aux;
+
+%{
+% By the NOAA Global Monitoring Division 
 aux = -(sind(latitude)*cosd(zenith)-sind(declination))/(cosd(latitude)*sind(zenith));
 aux = acosd(aux);
 azimuth = 180-aux;
+%}
+
+%aux = acosd((sind(declination)- cosd(zenith)*cosd(latitude))/(sind(zenith)*cosd(latitude)));
+
+%azimuth = sind(hour_angle)*abs(aux);
+
 
 end
 
